5b9cb5cb1af41e30af7fda90c340baff86d6702c
jzmtwong <jzmtwong@gmail.com>
2020-09-29 18:00:29 -0700

    progress

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 function getPlots(id) {
-    //Read samples.json
+    //read samples.json using d3
         d3.json("../../data/samples.json").then (sampledata =>{
             console.log(sampledata)
             var ids = sampledata.samples[0].otu_ids;
@@ -9,3 +9,27 @@ function getPlots(id) {
             console.log(sampleValues)
             var labels =  sampledata.samples[0].otu_labels.slice(0,10);
             console.log (labels);
+            // retrieve top 10 otu ids for the plot OTU. reverse order as well. 
+        var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+        // format OTU id's in chart
+            var OTU_id = OTU_top.map(d => "OTU " + d);
+            console.log(`OTU IDS: ${OTU_id}`)
+         // create labels for top 10 OTUs
+            var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+            console.log(`OTU_labels: ${labels}`);
+            //fill in trace for plot details
+            var trace = {
+                x: sampleValues,
+                y: OTU_id,
+                text: labels,
+                marker: {
+                color: 'blue'},
+                type:"bar",
+                orientation: "h",
+            };
+        };
+};
+//sanity check
+console.log(OTU_id)
+console.log(sampleValues)
+console.log(labels)
