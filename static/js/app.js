8739e6cd6ed049bfd5f954ceddba066c87a3202f
jzmtwong <jzmtwong@gmail.com>
2020-09-29 22:26:43 -0700

    bug fixing

--- static/js/app.js
+++ static/js/app.js
@@ -90,10 +90,9 @@ function getPanelInfo(id) {
 // store metadata in variable
         var metadata = data.metadata;
         console.log(metadata)
-
 // filter metadata by id
         var filtered = metadata.filter(meta => meta.id.toString() === id)[0];
-// select dempgrahic info to put in demographic panel
+// select demograhic info to put in demographic panel
         var panelInfo = d3.select('#sample-metadata')
 
 // clear demographic panel before getting new id input
@@ -103,6 +102,9 @@ function getPanelInfo(id) {
         Object.entries(filtered).forEach((key) => {   
             panelInfo.append("h5").text(key[0].toUpperCase() + ":  " + key[1] + "\n");    
         });
+//For the Bonus: use meta data to retrieve wfreq and generate gauge
+    getGauge(metadata.wfreq);
+    console.log(metadata.wfreq)
     })
 };
 
@@ -130,86 +132,9 @@ function initData() {
         // call the functions to display the data and the plots to the page
         getPlots(data.names[0]);
         getPanelInfo(data.names[0]);
-        getGauge(data.wfreq[0])
     });
 };
-//bonus
-//create gauge
-// reference to the code below:  I found a plot.ly blogger who found a good way to customize the gauge in plot.ly, url link below:
-//https://com2m.de/blog/technology/gauge-charts-with-plotly/
-// using that code to customize plot.ly
 
-//belly button cleaning function
-    //function entered must be between 0 and 180 to appear
-    // since frequency from 1-9, mutliples of 20 should work
-    function getGauge(wfreq) {
-        var level = parseFloat(wfreq)*20;
-        // Trig to calc meter point
-        var degrees = 180 - level,
-            radius = .5;
-        var radians = degrees * Math.PI / 180;
-        var x = radius * Math.cos(radians);
-        var y = radius * Math.sin(radians);
-        var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
-        // Path: may have to change to create a better triangle
-        var mainPath = path1,
-            pathX = String(x),
-            space = ' ',
-            pathY = String(y),
-            pathEnd = ' Z';
-        var path = mainPath.concat(pathX,space,pathY,pathEnd);
-    
-        var data = [{ type: 'scatter',
-        x: [0], y:[0],
-        marker: {size: 14, color:'850000'},
-        showlegend: false,
-        name: 'Freq',
-        text: level,
-        hoverinfo: 'text+name'},
-    
-        //modified values to match the sections of the gauge bar
-        // the gauge is acutally a pie chart, so made bottom half completely white to hide it from view
-        { values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-        rotation: 90,
-        text: ['8-9', '7-8', '6-7', '5-6', '4-5','3-4','2-3','1-2','0-1',''],
-        textinfo: 'text',
-        textposition:'inside',
-        marker: {colors:[
-            "rgba(0, 100, 10, .5)",
-            "rgba(10, 120, 20, .5)",
-            "rgba(50, 150, 0, .5)",
-            "rgba(100, 150, 20, .5)",
-            "rgba(170, 200, 40, .5)",
-            "rgba(202, 210, 60, .5)",
-            "rgba(210, 200, 80, .5)",
-            "rgba(230, 230, 100, .5)",
-            "rgba(240, 230, 215, .5)",
-            "rgba(255, 255, 255, 0)"
-        ]},
-        hoverinfo: 'label',
-        hole: .5,
-        type: 'pie',
-        showlegend: false
-        }];
-    
-        var layout = {
-        shapes:[{
-            type: 'path',
-            path: path,
-            fillcolor: '850000',
-            line: {
-            color: '850000'
-            }
-        }],
-        height: 400,
-        width: 400,
-        xaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]},
-        yaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]}
-        };
-    
-        Plotly.newPlot('gauge', data, layout);
-    };
+
 
 initData();
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,23 +1,3 @@
-// function for initial data 
-function initData() {
-
-    // select dropdown menu 
-    var dropdown = d3.select("#selDataset");
-
-    // read the data 
-    d3.json("static/js/samples.json").then((data)=> {
-        console.log(data)
-
-        // get the id data to the dropdown menu
-        data.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
-        });
-
-        // call the functions to display the data and the plots to the page
-        getPlots(data.names[0]);
-        getPanelInfo(data.names[0]);
-    });
-};
 //bonus
 //create gauge
 // reference to the code below:  I found a plot.ly blogger who found a good way to customize the gauge in plot.ly, url link below:
@@ -27,75 +7,74 @@ function initData() {
 //belly button cleaning function
     //function entered must be between 0 and 180 to appear
     // since frequency from 1-9, mutliples of 20 should work
-    function getGauge(wfreq) {
-        var level = parseFloat(wfreq)*20;
-        // Trig to calc meter point
-        var degrees = 180 - level,
-            radius = .5;
-        var radians = degrees * Math.PI / 180;
-        var x = radius * Math.cos(radians);
-        var y = radius * Math.sin(radians);
-        var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
-        // Path: may have to change to create a better triangle
-        var mainPath = path1,
-            pathX = String(x),
-            space = ' ',
-            pathY = String(y),
-            pathEnd = ' Z';
-        var path = mainPath.concat(pathX,space,pathY,pathEnd);
-    
-        var data = [{ type: 'scatter',
-        x: [0], y:[0],
-        marker: {size: 14, color:'850000'},
-        showlegend: false,
-        name: 'Freq',
-        text: level,
-        hoverinfo: 'text+name'},
-    
-        //modified values to match the sections of the gauge bar
-        // the gauge is acutally a pie chart, so made bottom half completely white to hide it from view
-        { values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-        rotation: 90,
-        text: ['8-9', '7-8', '6-7', '5-6', '4-5','3-4','2-3','1-2','0-1',''],
-        textinfo: 'text',
-        textposition:'inside',
-        marker: {colors:[
-            "rgba(0, 100, 10, .5)",
-            "rgba(10, 120, 20, .5)",
-            "rgba(50, 150, 0, .5)",
-            "rgba(100, 150, 20, .5)",
-            "rgba(170, 200, 40, .5)",
-            "rgba(202, 210, 60, .5)",
-            "rgba(210, 200, 80, .5)",
-            "rgba(230, 230, 100, .5)",
-            "rgba(240, 230, 215, .5)",
-            "rgba(255, 255, 255, 0)"
-        ]},
-        hoverinfo: 'label',
-        hole: .5,
-        type: 'pie',
-        showlegend: false
-        }];
-    
-        var layout = {
-        shapes:[{
-            type: 'path',
-            path: path,
-            fillcolor: '850000',
-            line: {
-            color: '850000'
-            }
-        }],
-        height: 400,
-        width: 400,
-        xaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]},
-        yaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]}
-        };
-    
-        Plotly.newPlot('gauge', data, layout);
+function getGauge(wfreq) {
+    var level = parseFloat(wfreq)*20;
+    // Trig to calc meter point
+    var degrees = 180 - level,
+        radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+    // Path: may have to change to create a better triangle
+    var mainPath = path1,
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    var data = [{ type: 'scatter',
+    x: [0], y:[0],
+    marker: {size: 14, color:'850000'},
+    showlegend: false,
+    name: 'Freq',
+    text: level,
+    hoverinfo: 'text+name'},
+
+    //modified values to match the sections of the gauge bar
+    // the gauge is acutally a pie chart, so made bottom half completely white to hide it from view
+    { values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6', '4-5','3-4','2-3','1-2','0-1',''],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:[
+        "rgba(0, 100, 10, .5)",
+        "rgba(10, 120, 20, .5)",
+        "rgba(50, 150, 0, .5)",
+        "rgba(100, 150, 20, .5)",
+        "rgba(170, 200, 40, .5)",
+        "rgba(202, 210, 60, .5)",
+        "rgba(210, 200, 80, .5)",
+        "rgba(230, 230, 100, .5)",
+        "rgba(240, 230, 215, .5)",
+        "rgba(255, 255, 255, 0)"
+    ]},
+    hoverinfo: 'label',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+    }];
+
+    var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+        color: '850000'
+        }
+    }],
+    height: 400,
+    width: 400,
+    xaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+                showgrid: false, range: [-1, 1]}
     };
+    Plotly.newPlot('gauge', data, layout);
+};
 
 
 
